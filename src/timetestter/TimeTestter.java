c32d3d93485ca20e8423257ab617f2f4049e8e56
PhongNT0107 <92393827+4uslander@users.noreply.github.com>
2022-08-12 21:23:46 +0700

    Time Tester

---
+++ src/timetestter/TimeTestter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetestter;
+
+/**
+ *
+ * @author LapTop
+ */
+public class TimeTestter {
+
+    public static void main(String[] args) {
+        NewClass n = new NewClass();
+        System.out.println(n.checkDate("2000","2", "29"));
+    }
+    
+}
